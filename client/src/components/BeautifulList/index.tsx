6571fef2525b06a40e6c80c23a8ff59e27574356
Nikita <neymarenko123@gmail.com>
2020-08-11 22:01:05 +0300

    fix about page
    make beautiful lists more addaptive

--- client/src/components/BeautifulList/index.tsx
+++ client/src/components/BeautifulList/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
+import "./stylesBeautifulList.scss"
 
 type Props = {
-    title:string,
+    title:string
     children:any
+    style?: React.CSSProperties
 }
 
-const BeautifulList:React.FC<Props> = ({title, children}) => (
-    <label className="about-label">
+const BeautifulList:React.FC<Props> = ({title, children, style}) => (
+    <label className="beautiful-label" style={style}>
         <input type="checkbox" className="about-checkbox"/>
         <div className="checkbox-content">
             <strong className="about-paragraph">{title}</strong><div className="triangle"/>
--- client/src/pages/AboutPage/index.tsx
+++ client/src/pages/AboutPage/index.tsx
@@ -22,19 +22,19 @@ const AboutPage = () => (
         <div className="about-content">
             <h2>A little bit more about me</h2>
             <div className="about-text">
-                <BeautifulList title="Education">
+                <BeautifulList title="Education" style={{width:'33%'}}>
                     <li>2005-2008 Kindergarten</li>
                     <li>2008-2015 School № 96</li>
                     <li>2015-2019 Lyceum № 142</li>
-                    <li>2015-Nowadays Igor Sikorsky Kyiv Polytechnic Institute</li>
+                    <li>2015-Nowadays Igor Sikorsky Kiev Polytechnic Institute</li>
                 </BeautifulList>
-                <BeautifulList title="Hobby">
+                <BeautifulList title="Hobby" style={{width:'33%'}}>
                     <li>Programming</li>
                     <li>Football</li>
                     <li>Volleyball</li>
                     <li>Math</li>
                 </BeautifulList>
-                <BeautifulList title="Character">
+                <BeautifulList title="Character" style={{width:'33%'}}>
                     <li>Purposeful</li>
                     <li>Team player</li>
                     <li>I like to improve myself and cats</li>
