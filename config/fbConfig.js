3f31351dbbfbf8bf7bbeea2213a9c324c81090e0
Nikita <neymarenko123@gmail.com>
2020-10-03 00:51:52 +0300

    Updates

--- config/fbConfig.js
+++ config/fbConfig.js
@@ -5,9 +5,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.images = exports.users = void 0;
 const firebase_admin_1 = __importDefault(require("firebase-admin"));
-const nikita_bludov_web_site_firebase_adminsdk_json_1 = __importDefault(require("./nikita-bludov-web-site-firebase-adminsdk.json"));
+const env_1 = __importDefault(require("../env"));
 firebase_admin_1.default.initializeApp({
-    credential: firebase_admin_1.default.credential.cert(nikita_bludov_web_site_firebase_adminsdk_json_1.default),
+    credential: firebase_admin_1.default.credential.cert(env_1.default.fb),
     databaseURL: "https://nikita-bludov-web-site.firebaseio.com"
 });
 exports.users = firebase_admin_1.default.firestore().collection("users");
