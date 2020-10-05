34bdb03233ec0ffe003dff1654aaae3059753f62
Nikita <neymarenko123@gmail.com>
2020-10-06 02:15:47 +0300

    Updates

--- env.js
+++ env.js
@@ -4,6 +4,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // tslint:disable-next-line:no-var-requires
 if (process.env.NODE_ENV !== 'production')
     require("dotenv").config();
+// @ts-ignore
 const env = {
     app: {
         port: process.env.APP_PORT,
@@ -20,7 +21,8 @@ const env = {
         type: process.env.FB_TYPE,
         project_id: process.env.FB_PROJECT_ID,
         private_key_id: process.env.FB_PRIVATE_KEY_ID,
-        private_key: process.env.FB_PRIVATE_KEY,
+        // @ts-ignore
+        private_key: process.env.FB_PRIVATE_KEY.replaceAll("\\n", "\n"),
         client_email: process.env.FB_CLIENT_EMAIL,
         client_id: process.env.FB_CLIENT_ID,
         auth_uri: process.env.FB_AUTH_URI,
