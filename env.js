68f8b26667caf9888f5bc976a30e166f1471640b
Nikita <neymarenko123@gmail.com>
2020-10-06 01:42:09 +0300

    Updates

--- env.js
+++ env.js
@@ -1,15 +1,10 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv_1 = __importDefault(require("dotenv"));
-const result = dotenv_1.default.config();
-if (result.error) {
-    throw result.error;
+if (process.env.NODE_ENV !== 'production') {
+    // @ts-ignore
+    import dotenv from 'dotenv';
+    dotenv_1.default.config();
 }
-console.log(result.parsed);
-console.log(process.env.APP_CORS_WHITELIST);
 const env = {
     app: {
         port: process.env.APP_PORT,
